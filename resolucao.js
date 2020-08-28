61fd1dfb37ec926fe6ec683f7913d65b68ecd012
robertvitoriano <robertvitorianodev@gmail.com>
2020-08-28 18:43:37 -0300

    wrpping solution up in a function

--- resolucao.js
+++ resolucao.js
@@ -4,12 +4,19 @@ const data = fs.readFileSync("./broken-database.json");
 
 const parsedData = JSON.parse(data);
 
-const decryptedData = parsedData.map(product => {
-    product.name = product.name.replace(/æ/g, 'a');
-    product.name = product.name.replace(/ø/g, 'o');
-    product.name = product.name.replace(/¢/g, 'c');
-    product.name = product.name.replace(/ß/g, 'b');
-    return product;
-});
+//  fixing names
+function fixNames(parsedData) {
+  const   decryptedData = parsedData.map(product => {
+        product.name = product.name.replace(/æ/g, 'a');
+        product.name = product.name.replace(/ø/g, 'o');
+        product.name = product.name.replace(/¢/g, 'c');
+        product.name = product.name.replace(/ß/g, 'b');
+        return product;
+    });
 
-console.log(decryptedData);
+
+return decryptedData;
+}
+
+console.log(fixNames(parsedData));
+
