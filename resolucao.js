8e0ed1144654d7c4edbe71c9f847bdb649d25257
robertvitoriano <robertvitorianodev@gmail.com>
2020-08-28 19:08:09 -0300

    informing zero quantity when is required

--- resolucao.js
+++ resolucao.js
@@ -2,21 +2,43 @@ const fs = require("fs");
 
 const data = fs.readFileSync("./broken-database.json");
 
-const parsedData = JSON.parse(data);
+const products = JSON.parse(data);
 
-//  fixing names
-function fixNames(parsedData) {
-  const   decryptedData = parsedData.map(product => {
-        product.name = product.name.replace(/æ/g, 'a');
-        product.name = product.name.replace(/ø/g, 'o');
-        product.name = product.name.replace(/¢/g, 'c');
-        product.name = product.name.replace(/ß/g, 'b');
-        return product;
-    });
-
-
-return decryptedData;
+// 1 Fixing names
+function fixNames(products) {
+  products.map((product) => {
+    product.name = product.name.replace(/æ/g, "a");
+    product.name = product.name.replace(/ø/g, "o");
+    product.name = product.name.replace(/¢/g, "c");
+    product.name = product.name.replace(/ß/g, "b");
+  });
 }
 
-console.log(fixNames(parsedData));
+fixNames(products);
+//************************************************************************************************************************************ */
 
+// 2 converting price to number
+
+function convertPricesToNumber(products) {
+  products.map((product) => {
+    product.price = parseFloat(product.price);
+  });
+}
+
+convertPricesToNumber(products);
+
+//************************************************************************************************************************************ */
+
+// 3 inform 0 quantity
+
+function informZeroquantity (products){
+    products.map((product) => {
+        if (!product.quantity) {
+            product.quantity = 0;
+        }
+    });
+}
+
+informZeroquantity(products);
+
+console.log(products);
